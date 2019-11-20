c685fa0a749188644910807df655188a4f117148
vishnu-thiyagarajan <33596700+vishnu-thiyagarajan@users.noreply.github.com>
2019-11-20 14:40:33 +0530

    Update fileservice.js

--- fileservice.js
+++ fileservice.js
@@ -1,5 +1,5 @@
-var http = require("http");
-var fs = require("fs");
+let http = require("http");
+let fs = require("fs");
 
 function send404Response(response){
     response.writeHead(404, {"Content-Type": "text/plain"});
@@ -8,14 +8,26 @@ function send404Response(response){
 }
 
 function onRequest(request, response) {
-
-    if( request.method == 'GET' && request.url.startsWith('/') && fs.existsSync(request.url.slice(1))){
+    if (request.url=="/" || request.url=="" ) request.url="/make"
+    let foldsInDirectory, filesHtml = ""
+    let file = /^\/.*\/(.*\..*)$/g.exec(request.url)
+    if( file && request.method == 'GET' && fs.existsSync(request.url.slice(1))){
         response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("content of the file is : <br>")
         fs.createReadStream("."+request.url).pipe(response);
+    }else{
+    if (!file && request.method == 'GET') {
+        foldsInDirectory = fs.readdirSync(request.url.slice(1))
+        for (let folderName of foldsInDirectory){
+            filesHtml += "<li> <a href='http://localhost:8000"+request.url+"/"+folderName+"'>"+ folderName + "</a></li>"
+        }
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("files in the directory : <br>" + filesHtml)
+        response.end()
     }else{
         send404Response(response);
     }
-
+    }
 }
 let server = http.createServer(onRequest)
 server.listen(8000)
@@ -23,4 +35,4 @@ console.log("Server is up...")
 setTimeout(() => {
     server.close();
     console.log("Server is down...")
-},10000)
+},900000)
